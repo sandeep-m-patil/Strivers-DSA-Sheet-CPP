d85b01bb76e09bf356bd505dfc20592dfd62caf5
sandeep <sandeeppatil10705@gmail.com>
2025-02-25 19:24:31 +0530

    updated

---
+++ Assignments/Arrays/Is-Sorted-Average-Time-Complexity.cpp
@@ -0,0 +1,26 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Best Time Complexity is O(n^2) [inefficient]
+bool isSorted(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[i])
+        return false;
+    }
+  }
+
+  return true;
+}
+
+int main() {
+
+  int arr[] = {1, 2, 3, 4, 5}, n = 5;
+  bool ans = isSorted(arr, n);
+  if (ans) cout << "True" << endl;
+  else cout << "False" << endl;
+  return 0;
+}
+
